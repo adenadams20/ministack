770924b6258abdd4a44118dca279011b90694234
sene32304@gmail.com <sene32304@gmail.com>
2025-10-15 22:33:19 +0000

    initialise sadio

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
-import Login from "./pages/login";
-import CadQuestionFirebase from "./components/QuestionCard";
-import QuestionListeFirebase from "./components/QuestonList";
-import SearchBar from "./components/SearchBar";
+import React from "react";
+
 
 function App() {
-  const [user, setUser] = useState(null);
-  const [search, setSearch] = useState("");
+  
 
   return (
-    <div className="max-w-2xl mx-auto mt-10">
-      {!user ? (
-        <Login setUser={setUser} />
-      ) : (
-        <>
-          <p className="mb-4 text-gray-700">Connecté en tant que <b>{user.email}</b></p>
-          <CadQuestionFirebase />
-          <SearchBar search={search} setSearch={setSearch} />
-          <QuestionListeFirebase search={search} />
-        </>
-      )}
-    </div>
-  );
+   <div>
+   </div>
+  )
 }
 
 export default App;
