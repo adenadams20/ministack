242608e2cc5cec2f4b1df9ee14c0e463857d2cb2
byadiallo <zayabel99@gmail.com>
2025-10-21 10:10:28 +0000

    Commit pour test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="relative">
-      <Navbar />
-      <Sidebar />
+    <div >
 
-      {/* Contenu principal */}
-      <main className="md:ml-56 mt-[64px] p-4">
-      </main>
-      <Footer />
     </div>
   );
 }
