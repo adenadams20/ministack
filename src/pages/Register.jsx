b0043df7cfbc2784a874d33c8523fa99ea9ac7e7
adenadams20 <adamsdiene@gmail.com>
2025-10-29 01:20:04 +0000

    final

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -51,20 +51,20 @@ export default function Register() {
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center bg-gray-200 px-6 py-18 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight ">
+        <h2 className="mt-10 text-center text-2xl font-bold text-gray-800 tracking-tight ">
           Create your account
         </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full bg-blue-950 p-2 shadow-xl/30 py-4 sm:max-w-sm">
+      <div className="mt-10 sm:mx-auto sm:w-full bg-white p-2 shadow-xl/30 py-4 sm:max-w-sm">
         {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
         {message && <p className="text-green-500 text-sm mb-2">{message}</p>}
 
         <form onSubmit={handleRegister} className="space-y-6">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium  ">
+            <label htmlFor="email" className="block text-sm  text-gray-600  font-medium  ">
               Email address
             </label>
             <input
@@ -73,12 +73,12 @@ export default function Register() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="block w-full border-2 rounded-md  px-3 py-1.5 bg-white placeholder:text-gray-500"
+              className="block w-full border-2 rounded-md  px-3 py-1.5 bg-gray-100 placeholder:text-gray-500"
             />
           </div>
 
           <div>
-            <label htmlFor="password" className="block text-sm font-medium ">
+            <label htmlFor="password" className="block text-sm  text-gray-600 font-medium ">
               Password
             </label>
             <input
@@ -87,11 +87,11 @@ export default function Register() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="block w-full border-2 rounded-md bg- px-3 py-1.5  placeholder:text-gray-500 mt-2 bg-white"
+              className="block w-full  border-2 rounded-md bg-gray-100 px-3 py-1.5  placeholder:text-gray-500 mt-2 "
             />
           </div>
           <div>
-            <label htmlFor="confirmPassword" className="block text-sm font-medium  ">
+            <label htmlFor="confirmPassword" className="block text-sm text-gray-600  font-medium  ">
               Confirm password
             </label>
             <input
@@ -100,13 +100,13 @@ export default function Register() {
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               required
-              className="block w-full border-2 rounded-md 0 px-3 py-1.5 bg-white  placeholder:text-gray-500 mt-2"
+              className="block w-full border-2 rounded-md 0 bg-gray-100 px-3 py-1.5 focus:outline-none focus:ring-blue-500  placeholder:text-gray-500 mt-2"
             />
           </div>
 
           <button
             type="submit"
-            className="w-full rounded-md px-3 py-2  font-semibold "
+            className="w-full rounded-md px-3 py-2  hover:text-gray-500 text-amber-50 bg-blue-600  font-semibold "
           >
             Créer un compte
           </button>
@@ -114,9 +114,9 @@ export default function Register() {
 
         
 
-        <p className="mt-6 text-center text-sm ">
+        <p className="mt-6 text-center text-sm text-gray-500  ">
           Already have an account?{" "}
-          <Link to="/" className="font-semibold  hover:text-indigo-300">
+          <Link to="/" className="font-semibold text-blue-600   hover:text-indigo-300">
             Sign In
           </Link>
         </p>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -43,14 +43,14 @@ export default function Login() {
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 bg-gray-200 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-black">
           Sign in to your account
         </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow-xl/30 p-3 bg-lime-50">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm shadow-xl/30 p-3 bg-white">
         {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
 
         <form onSubmit={handleLogin} className="space-y-6">
