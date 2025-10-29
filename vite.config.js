d63034e71fbf0edc2361c6c6f67b92883aacad42
adenadams20 <adamsdiene@gmail.com>
2025-10-29 03:09:45 +0000

    final2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ministack/', // 👈 Remplace par le nom exact de ton repo
+
+
   plugins: [
         tailwindcss(),
 
