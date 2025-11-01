5472aac94fd62f268cd2ec6d0e98e7b2f0f84ac0
adenadams20 <adamsdiene@gmail.com>
2025-11-01 19:03:58 +0000

    'var'

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -11,15 +11,14 @@ import { getFirestore } from "firebase/firestore";
 
 // ✅ Configuration Firebase à partir des variables d'environnement
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBa-QobWSIZkRRA8a4c5W_4VIEuL3RFGQM",
+  authDomain: "ministack-9175c.firebaseapp.com",
+  projectId: "ministack-9175c",
+  storageBucket: "ministack-9175c.firebasestorage.app",
+  messagingSenderId: "758710672171",
+  appId: "1:758710672171:web:0757f10caa31f868ad436e",
+  measurementId: "G-Z88W320XKY"
 };
-
 // ✅ Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
